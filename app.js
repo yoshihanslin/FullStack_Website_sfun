3bdbd251273b95c99d3b6f630b3f316a90043cdb
Cheng-Han Lin <yoshihanslin@gmail.com>
2017-10-03 21:38:14 -0500

    upload to github

---
+++ app.js
@@ -0,0 +1,63 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
+    
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+ 
+mongoose.Promise = global.Promise;
+
+//on local host eg cloud9: (use any name for DB in localhost/DB), optional: {useMongoClient}
+//mongoose.connect("mongodb://localhost/Website", {useMongoClient: true}); // 
+//use following when deployed to online server, eg heroku, with database hosted at mlab.com
+//mongoose.connect(mongodb://<dbuser>:<dbpassword>@dsport.mlab.com:port/website_name
+//on heroku config: DATABASEURL=mongodb://<dbuser>:<dbpassword>@dsport.mlab.com:port/website_name
+var url = process.env.DATABASEURL || "mongodb://localhost/Website";
+mongoose.connect(url, {useMongoClient: true});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); //seed the database
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// these made available to every js file:
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The Summer Fun Server Has Started!");
+});
